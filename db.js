1798dd9d6c372cd462d68c3d858f700142749096
kaquoc <kaquoc1206@gmail.com>
2022-08-17 15:38:13 -0400

    added DATABASE_URL without SSL connection between local host -> herokupostgres

--- db.js
+++ db.js
@@ -4,6 +4,9 @@ import pg from 'pg';
 
 const {Pool} = pg;
 
+import {database_url} from './config.js';
+
+
 //dependent on where we are running the server: either Heroku or local
 
 //currently running local with PostGres user kaka and database vleague with table mock_data
@@ -16,8 +19,10 @@ const poolConfig2 = {
 };
 
 //config to run with Heroku Postgres. 
+
+
 const poolConfig = {
-    connectionString: process.env.DATABASE_URL,
+    connectionString: database_url,
     //HerokuPostgres requires SSL connection to be made. setting rejectUnauthorized to false means we are bypassing this
     //https://stackoverflow.com/questions/61097695/self-signed-certificate-error-during-query-the-heroku-hosted-postgres-database
     ssl: {

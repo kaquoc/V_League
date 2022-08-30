76ba45633f046e720a9c062e1bacc5a14cf6fae5
kaquoc <kaquoc1206@gmail.com>
2022-08-30 14:30:11 -0400

    function to increment players appearance

--- update.js
+++ update.js
@@ -14,7 +14,7 @@ import pool from './db.js';
 */
 const updatePlayer_goal = async (player_name, goals) => {
     const query = `UPDATE "players" 
-                   SET "goals" = goals - $2
+                   SET "goals" = goals + $2
                    WHERE "player_name" = $1`;
     try {
         await pool.connect();          // gets connection
@@ -27,10 +27,26 @@ const updatePlayer_goal = async (player_name, goals) => {
         await pool.end();              // closes connection
     }
 };
+/**increament a player appearance 1 at a time */
+const updatePlayer_appearance = async (player_name) => {
+    const query = `UPDATE "players" 
+                   SET "appearance" = appearance - 1
+                   WHERE "player_name" = $1`;
+    try {
+        await pool.connect();          // gets connection
+        await pool.query(query, [player_name]); // sends queries
+        return true;
+    } catch (error) {
+        console.error(error.stack);
+        return false;
+    } finally {
+        await pool.end();              // closes connection
+    }
+};
 
 
 
-updatePlayer_goal('Pham Tuan Hai','2').then(result => {  // userName, userRole, userId
+updatePlayer_appearance('Pham Tuan Hai').then(result => {  // userName, userRole, userId
     if (result) {
         console.log('player updated');
     }

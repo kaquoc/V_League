492b36240dfa29e1207a73d76dfdfea047e7cf42
kaquoc <kaquoc1206@gmail.com>
2022-08-30 17:51:29 -0400

    changes to updateMatchResult func, add wins, lose, draw updates

--- update.js
+++ update.js
@@ -58,28 +58,45 @@ const updatePlayer_kit = async (player_name,kit_num) => {
         await pool.end();              // closes connection
     }
 };
-
+/**Update standings Table, 
+ * UPDATE home_team points + wins
+ * UPDATE away_team points + loses
+ */
 const updateMatchResult = async (home_team,score1,away_team,score2) => {
-    let query = '';
+    let query = ''; //for home team
+    let query2 = ''; // for away team
     let supply = [];
+    let supply2 = [];
     if (score1 > score2){ //home team wins
         query = `UPDATE "standings" 
-                   SET "points" = points + 3
+                   SET "points" = points + 3, "wins" = wins+1
                    WHERE "team_name" = $1`;
+        query2 = `UPDATE "standings" 
+                   SET "lose" = lose + 1
+                   WHERE "team_name" = $1`;
+
         supply = [home_team];
+        supply2 = [away_team];
     } else if (score2 > score1){//away team wins
         query = `UPDATE "standings" 
-                   SET "points" = points + 3
+                   SET "points" = points + 3,"wins" = wins+1
+                   WHERE "team_name" = $1`;
+        query2 = `UPDATE "standings" 
+                   SET "lose" = lose + 1
                    WHERE "team_name" = $1`;
         supply = [away_team];
+        supply2 = [home_team];
     }else{ //draw
         query = 'UPDATE "standings" SET "points" = points + 1 WHERE "team_name" = $1 OR "team_name" = $2';
+        query2 = 'UPDATE "standings" SET "draw" = draw + 1 WHERE "team_name" = $1 OR "team_name" = $2';
         supply = [home_team,away_team];
+        supply2 = [home_team,away_team];
     }
    
     try {
         await pool.connect();          // gets connection
         await pool.query(query, supply); // sends queries
+        await pool.query(query2,supply2);
         return true;
     } catch (error) {
         console.error(error.stack);
@@ -91,7 +108,7 @@ const updateMatchResult = async (home_team,score1,away_team,score2) => {
 
 
 
-updateMatchResult('Hanoi',1, 'Sai Gon',1).then(result => {
+updateMatchResult('Hanoi',1, 'Sai Gon',2).then(result => {
     if (result){
         console.log("update success");
     }

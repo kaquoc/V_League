ca50979c81924ac26660f6b1df78456e436503ab
kaquoc <kaquoc1206@gmail.com>
2022-08-30 14:12:15 -0400

    add function to update player goals attribute in DB

--- update.js
+++ update.js
@@ -2,6 +2,36 @@
  * What needs to change after each round:
  *     - Standings table
  *          - team's point, wins, loses, draw, match_played
+ *     - Players table
+ *          - kit_number, player_name, team_name, appearance, goals, position, age.
  * 
  * 
  */
+import pool from './db.js';
+
+/** How to update player
+ *  Player name -> find, 
+*/
+const updatePlayer_goal = async (player_name, goals) => {
+    const query = `UPDATE "players" 
+                   SET "goals" = goals - $2
+                   WHERE "player_name" = $1`;
+    try {
+        await pool.connect();          // gets connection
+        await pool.query(query, [player_name, goals]); // sends queries
+        return true;
+    } catch (error) {
+        console.error(error.stack);
+        return false;
+    } finally {
+        await pool.end();              // closes connection
+    }
+};
+
+
+
+updatePlayer_goal('Pham Tuan Hai','2').then(result => {  // userName, userRole, userId
+    if (result) {
+        console.log('player updated');
+    }
+});

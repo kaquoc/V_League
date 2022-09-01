cc8287a0a94c64525fc658085cdc56df81adace2
kaquoc <kaquoc1206@gmail.com>
2022-09-01 12:13:37 -0400

    added Thanh Hoa team

---
+++ V_League_Teams/Thanh_Hoa.sql
@@ -0,0 +1,43 @@
+/*Thanh Hoa*/
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (25,'Nguyen Thanh Diep', 'Thanh Hoa',9,0,'GK',30);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (26,'Tran Buu Ngoc', 'Thanh Hoa',4,0,'GK',31);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (16,'Dinh Tien Thanh', 'Thanh Hoa',6,0,'CB',31);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (34,'Dao Ngoc Tan', 'Thanh Hoa',13,0,'RB',27);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (95,'Gustavo', 'Thanh Hoa',5,0,'CB',27);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (98,'Nguyen Huu Lam', 'Thanh Hoa',3,0,'CB',24);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (15,'Trinh Van Loi', 'Thanh Hoa',13,1,'CB',27);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (4,'Dam Tien Dung', 'Thanh Hoa',13,1,'LB',26);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (32,'Le Ngoc Nam', 'Thanh Hoa',5,0,'CM',29);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (11,'Le Pham Thanh Long', 'Thanh Hoa',13,0,'CM',26);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (19,'Le Quoc Phuong', 'Thanh Hoa',10,0,'LW',31);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (10,'Le Van Thang', 'Thanh Hoa',13,2,'RW',32);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (9,'Le Xuan Hung', 'Thanh Hoa',6,0,'RM',30);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (29,'Ngoc Ha Doan', 'Thanh Hoa',5,0,'CM',18);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (7,'Nguyen Huu Dung', 'Thanh Hoa',3,0,'DM',27);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (5,'Nguyen Minh Tung', 'Thanh Hoa',9,1,'CB',30);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (20,'Nguyen Trong Hung', 'Thanh Hoa',11,0,'LM',24);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (8,'Pinto Paulo', 'Thanh Hoa',12,3,'CM',28);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (3,'Vu Xuan Cuong', 'Thanh Hoa',7,0,'LB',30);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (99,'Gustavo Balotelli', 'Thanh Hoa',2,1,'LW',26);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (2,'Hoang Dinh Tung', 'Thanh Hoa',5,1,'CF',30);

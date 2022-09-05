14a0ba2b55dc6a1ea67bf132883ed2530ae06fc5
kaquoc <kaquoc1206@gmail.com>
2022-09-05 15:45:02 -0400

    added Viettel FC

---
+++ V_League_Teams/Viettel.sql
@@ -0,0 +1,52 @@
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (26,'Tran Nguyen Manh', 'Viettel',11,0,'GK',30);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (25,'Quang The Tai', 'Viettel',3,0,'GK',25);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (3,'Nguyen Thanh Binh', 'Viettel',13,0,'CB',21);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (4,'Bui Tien Dung', 'Viettel',5,1,'CB',26);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (20,'Cao Tran Hoang Dung', 'Viettel',6,1,'CB',23);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (18,'Hoang Son Tran', 'Viettel',2,0,'CF',25);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (15,'Nguyen Xuan Kien', 'Viettel',10,1,'LB',22);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (2,'Phan Tuan Tai', 'Viettel',5,0,'LB',21);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (5,'Truong Van Thiet', 'Viettel',4,0,'CB',27);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (11,'Abdumuminov Jahongir', 'Viettel',14,2,'CM',29);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (88,'Bui Duy Thuong', 'Viettel',14,0,'RM',26);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (6,'Dinh Tuan Tai', 'Viettel',2,0,'CM',23);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (33,'Duong Van Hao', 'Viettel',10,0,'RW',25);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (94,'Magno Geovane', 'Viettel',14,6,'AM',28);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (21,'Nguyen Duc Chien', 'Viettel',7,0,'DM',24);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (28,'Nguyen Hoang Duc', 'Viettel',14,2,'CM',24);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (17,'Nguyen Duc Hoang Minh', 'Viettel',11,2,'CM',24);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (29,'Truong Tien Anh', 'Viettel',11,0,'LW',23);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (8,'Nguyen Huu Thang', 'Viettel',3,0,'CM',22);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (10,'Pedro Paulo', 'Viettel',10,2,'CF',28);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (14,'Bui Quang Khai', 'Viettel',7,0,'AM',29);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (12,'Ho Khac Ngoc', 'Viettel',10,0,'DM',30);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (99,'Nham Manh Dung', 'Viettel',7,1,'CF',22);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (19,'Tran Danh Trung', 'Viettel',9,0,'CF',21);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (7,'Lemes Caique', 'Viettel',3,0,'CF',29);
+INSERT INTO players (kit_number, player_name,team_name,appearance,goals,position, age) 
+											VALUES (22,'Nguyen Si Chien', 'Viettel',1,0,'AM',23);

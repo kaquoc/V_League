b70efd948218377872fec166e54903ce668e2653
kaquoc <kaquoc1206@gmail.com>
2023-01-17 22:31:30 -0500

    finish upload all team data

--- web_scrapper/craw.js
+++ web_scrapper/craw.js
@@ -13,8 +13,8 @@ import cheerio from 'cheerio';
 import fs from 'fs';
 
 /** CONSTANT VARIABLE */
-const team_url = 'https://vpf.vn/team/hong-linh-ha-tinh/?sid=64431';
-const TEAM= "Hong Linh Ha Tinh"
+const team_url = 'https://vpf.vn/team/nam-dinh/?sid=64431';
+const TEAM= "Thep Xanh Nam Dinh"
 const CURRENT_YEAR = 2023;
 
 

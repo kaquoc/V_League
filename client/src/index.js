d97956bca34ad3f27014942efab6f15e54b08bca
kaquoc <kaquoc1206@gmail.com>
2023-01-31 21:26:46 -0500

    added css style sheet and side bar in html

--- client/src/index.js
+++ client/src/index.js
@@ -7,12 +7,29 @@ import ReactDOM from 'react-dom';
 
 //JSX: writing HTML with React
 
-const my_element = <h1>I Love JSX!</h1>;
-const root = ReactDOM.render(my_element, document.getElementById('root'));
+var my_element = <h1>I Love JSX!</h1>;
+
 
 /**Without JSX
     const myElement = React.createElement('h1', {}, 'I do not use JSX!');
     const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(myElement);
  */
+
+/**Inserting large block of HTML
+ * Some rules:
+ * - HTML code must be wrapped in ONE top level element
+ * - Element must be closed with />
+ * 
+ * 
+*/
+my_element = (
+  <ul>
+    <li>Apples</li>
+    <li>Bananas</li>
+    <li>Cherries</li>
+  </ul>
+);
+
+//ReactDOM.render(my_element, document.getElementById('root'));
 

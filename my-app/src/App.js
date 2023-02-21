959f29ecbc1844bfd00ea3a18457422c2f5806c8
kaquoc <kaquoc1206@gmail.com>
2023-02-20 19:42:07 -0500

    added top nav bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+function Topnav(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class = "topNavBar">
+      <a>V League API</a>
+        <div class = "right-text">
+          <a >About</a>
+          <a >Code</a>
+        </div>
     </div>
   );
 }
+function App() {
+  return (
+    <>
+    <Topnav />
+    </>
+  )
+    
+}
+
 
 export default App;
